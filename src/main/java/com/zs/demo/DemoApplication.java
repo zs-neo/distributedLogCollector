ba3e11125a2d12da90fe01ac3bfeff475f9beb5f
代码dou队 <17397323269@163.com>
2020-05-30 15:59:06 +0800

    初始提交

--- src/main/java/com/zs/demo/DemoApplication.java
+++ src/main/java/com/zs/demo/DemoApplication.java
@@ -1,11 +1,15 @@
 package com.zs.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {KafkaAutoConfiguration.class})
 public class DemoApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
---
+++ src/main/java/com/zs/demo/controller/DemoController.java
@@ -0,0 +1,30 @@
+/*
+ * Author github: https://github.com/zs-neo
+ * Author Email: 2931622851@qq.com
+ */
+package com.zs.demo.controller;
+
+import com.zs.demo.DemoApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhousheng
+ * @version 1.0
+ * @since 2020/5/30 15:48
+ */
+@RestController
+@RequestMapping("/api")
+public class DemoController {
+	
+	private static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+	
+	@RequestMapping("/hello")
+	public void hello() throws Exception {
+		logger.info("invoke hello!");
+		throw new Exception("test invoke hello occur exception");
+	}
+	
+}
